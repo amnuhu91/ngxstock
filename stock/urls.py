c7e430b4c0ab32012f511482fff82646d95fa528
amnuhu91 <amnuhu91@yahoo.com>
2024-01-06 11:52:32 +0100

    whit

--- stock/urls.py
+++ stock/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import TemplateView
 from prediction.views import make_prediction
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path('predict/', make_prediction,name="prediction"),
     
     
-]
+]+ + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
